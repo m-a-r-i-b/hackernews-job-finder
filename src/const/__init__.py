a94c2a82f5604a488cec9d5d50a2806be49398b5
Marib Sultan <g.maribsultan@gmail.com>
2024-06-27 01:07:34 +0500

    initial commit

---
+++ src/const/__init__.py
@@ -0,0 +1,4 @@
+RAW_COMMENTS_DIR="raw_comments"
+CRITERIA_FILTERED_COMMENTS="criteria_filtered_comments"
+
+CRITERIA_FILE_PATH="criteria"
---
+++ src/main.py
@@ -0,0 +1,4 @@
+from dotenv import load_dotenv
+load_dotenv(".env")
+
+from criteria_filter import criteria_filter
